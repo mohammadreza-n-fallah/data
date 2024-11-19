211ce0a5f6fde6274ce6a0ae96271b95d6b4195d
mohammadreza-n-fallah <mohammadrezafalah0987@gmail.com>
2024-11-19 16:31:18 +0330

    develop

---
+++ search/views.py
@@ -0,0 +1,30 @@
+from django.shortcuts import render
+from sqlalchemy import *
+# Create your views here.
+import pandas as pd
+
+
+def db(request):
+    con=create_engine("mssql://@localhost/vvv?driver=ODBC Driver 17 for SQL Server")
+    conn=con.connect()
+   
+    # metadata = MetaData() #extracting the metadata
+    # datat= Table('FSI', metadata,             
+    # autoload_with=con) #Table object
+    # re=pd.read_sql_query("SELECT * FROM FSI",conn)
+    # query = datat.select() #SELECT * FROM divisions
+    # print(query)
+    ff=request.POST.get("search","")
+    yy=request.POST.get("year","")
+    print(ff)
+    dddd="FSI"
+    if ff and yy:
+        st=text(f"SELECT * FROM {dddd} WHERE (Country='{ff}' AND Year='{yy}') ")
+        rs = conn.execute(st)
+        print(rs)
+        return render(request,"index.html",{"re":rs})
+    else:    
+        gh=text(f"SELECT * FROM {dddd} WHERE Country='{ff}'")
+        kl = conn.execute(gh)
+  
+        return render(request,"index.html",{'kl':kl})
